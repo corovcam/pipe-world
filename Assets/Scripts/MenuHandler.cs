59d901a1a27d51e583d7591bf11637cc4882a0c8
Martin Čorovčák <martino.coro@gmail.com>
2022-08-02 14:46:00 +0200

    Add Title to MainMenu

--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -25,20 +25,28 @@ public class MenuHandler : MonoBehaviour
         GenerateBtn("Level Select", 0, 150);
         GenerateBtn("Arcade", 0, -150);
 
-        //// Text
-        //myText = new GameObject();
-        //myText.transform.parent = canvasGO.transform;
-        //myText.name = "wibble";
+        // Title Text
+        GameObject titleGO = new GameObject();
+        titleGO.transform.parent = canvasGO.transform;
+        titleGO.layer = canvasGO.layer;
+        titleGO.name = "Plumber";
 
-        //text = myText.AddComponent<Text>();
-        //text.font = (Font)Resources.Load("MyFont");
-        //text.text = "wobble";
-        //text.fontSize = 100;
+        TextMeshProUGUI textComp = titleGO.AddComponent<TextMeshProUGUI>();
+        textComp.text = "Plumber";
+        textComp.font = (TMP_FontAsset)Resources.Load("UI/Electronic Highway Sign SDF");
+        textComp.fontSize = 40;
+        textComp.fontStyle = FontStyles.Bold;
+        textComp.alignment = TextAlignmentOptions.Center;
+        textComp.enableWordWrapping = false;
+        textComp.color = Color.white;
 
-        //// Text position
-        //rectTransform = text.GetComponent<RectTransform>();
-        //rectTransform.localPosition = new Vector3(0, 0, 0);
-        //rectTransform.sizeDelta = new Vector2(400, 200);
+        titleGO.AddComponent<CanvasRenderer>();
+
+        // Title text position
+        RectTransform transform = textComp.GetComponent<RectTransform>();
+        transform.localPosition = new Vector3(0, 400, 0);
+        transform.sizeDelta = new Vector2(200, 50);
+        transform.localScale = new Vector3(2.5f, 2.5f, 0);
     }
 
     void GenerateBtn(string txt, int posX, int posY)
@@ -60,7 +68,7 @@ public class MenuHandler : MonoBehaviour
         transform.sizeDelta = new Vector2(160, 30);
         transform.localScale = new Vector3(4.5f, 4.5f, 0);
 
-        // Text
+        // Button Text
         GameObject textGO = new GameObject();
         textGO.transform.parent = buttonGO.transform;
         textGO.layer = buttonGO.layer;
@@ -72,6 +80,7 @@ public class MenuHandler : MonoBehaviour
         textComp.alignment = TextAlignmentOptions.Center;
         textComp.enableWordWrapping = false;
         textComp.color = Color.black;
+
         textGO.AddComponent<CanvasRenderer>();
 
         transform = textComp.GetComponent<RectTransform>();
